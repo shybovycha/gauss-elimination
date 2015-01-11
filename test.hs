0afbd8696c20a07073568bba08f892228af59082
Artem Shoobovych <shybovycha@gmail.com>
2015-01-11 13:04:40 +0100

    Removed test file

--- test.hs
+++
@@ -1,7 +0,0 @@
-split :: (Eq a) => a -> [a] -> [[a]]
-split _ [] = []
-split d s = [word] ++ split d xs'
-	where
-		word = takeWhile (/= d) s
-		xs = drop (length word) s
-		xs' = dropWhile (== d) xs
